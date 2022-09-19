634d6d5b28f58a7db383a2f687c2b48102274c53
FX Wood <fxwood2@gmail.com>
2022-09-19 00:01:50 -0700

    bunch of changes

--- nvim/after/plugin/keymap.lua
+++ nvim/after/plugin/keymap.lua
@@ -1,6 +1,7 @@
 print('loading keymaps')
 -- shamelessly stolen from theprimeagen
 -- https://github.com/ThePrimeagen/.dotfiles/blob/61784572c81d7a1973f6c0a2a4930c6739c89cc9/nvim/.config/nvim/after/plugin/keymap/init.lua
+-- see also: https://neovim.io/doc/user/map.html#map-overview
 local Remap = require("fxwood.keymap")
 local nnoremap = Remap.nnoremap
 local vnoremap = Remap.vnoremap
@@ -8,6 +9,7 @@ local inoremap = Remap.inoremap
 local xnoremap = Remap.xnoremap
 local noremap = Remap.noremap
 local nmap = Remap.nmap
+local noremap_bang = Remap.noremap_bang
 
 -- for this to work on mac your terminal must send the right key
 -- in iTerm, make the left and/or the right option key send "Esc+" in settings
@@ -23,3 +25,14 @@ noremap("<up>", "<nop>")
 noremap("<down>", "<nop>")
 noremap("<left>", "<nop>")
 noremap("<right>", "<nop>")
+
+nnoremap("<leader>f", function()
+    require('telescope.builtin').find_files()
+end)
+nnoremap("<leader>g", function()
+    require('telescope.builtin').git_files()
+end)
+
+noremap("<leader><leader>", "<Esc>")
+noremap_bang("<leader><leader>", "<Esc>")
+
