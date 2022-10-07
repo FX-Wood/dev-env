2c84be3da796fae9ca924c4e18374a46e351f97a
FX Wood <fxwood2@gmail.com>
2022-10-07 16:04:05 -0700

    uncomment all the things

--- install-scripts/neovim-raspbian.sh
+++ install-scripts/neovim-raspbian.sh
@@ -1,9 +1,9 @@
 echo "raspberry pi detected, building neovim from source"
 echo "installing build deps"
-# sudo apt-get -y install  ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip curl doxygen
-# git clone https://github.com/neovim/neovim
-# cd neovim && git checkout stable
-# make CMAKE_BUILD_TYPE=Release
-# sudo make install
-# sudo ln -s /usr/local/bin/nvim /usr/bin/nvim
-# sudo update-alternatives --install /usr/bin/editor editor /usr/local/bin/nvim 1
+sudo apt-get -y install  ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip curl doxygen
+git clone https://github.com/neovim/neovim
+cd neovim && git checkout stable
+make CMAKE_BUILD_TYPE=Release
+sudo make install
+sudo ln -s /usr/local/bin/nvim /usr/bin/nvim
+sudo update-alternatives --install /usr/bin/editor editor /usr/local/bin/nvim 1
