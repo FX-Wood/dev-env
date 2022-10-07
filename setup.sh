bd8c79663bbe45603dd6202e856f388a278cb742
FX Wood <fxwood2@gmail.com>
2022-10-07 15:27:04 -0700

    break out neovim compilation into script

--- setup.sh
+++ setup.sh
@@ -1,35 +1,39 @@
 # this script requires https://github.com/fx-wood/dev-env
 # to be cloned into ~/
 
-# install nvim
+configure_nvim() {
+    echo "linking nvim config"
+    # mkdir -p ~/.config
+    # sudo rm -rf ~/.config/nvim
+    # ln -s ~/dev-env/nvim ~/.config
+    # nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
+}
+
+install_zsh() {
+    echo "installing zsh and oh-my-zsh"
+    # sudo apt-get update && sudo apt-get upgrade 
+    # sudo apt-get -y install zsh
+    # sudo chsh -s /bin/zsh $USER
+    # # install oh-my-zsh
+    # sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+    # sudo rm -rf ~/.zshrc
+    # sudo rm -rf ~/.zshrc.pre-oh-my-zsh
+    # ln -s ~/dev-env/shell/.zshrc ~/.zshrc
+}
+
+install_docker_engine() {
+    echo "installing docker"
+    # sh -c "$(curl -fsSL https://get.docker.com)"
+    sudo usermod -aG docker $USER
+}
 if grep -iq "raspberry" /proc/device-tree/model > /dev/null 2>&1; then
-	echo "checking if we're on a raspberry pi"
-	echo "raspberry pi detected, building neovim from source"
-	echo "installing build deps"
-	sudo apt-get -y install  ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip curl doxygen
-	git clone https://github.com/neovim/neovim
-	cd neovim && git checkout stable
-	make CMAKE_BUILD_TYPE=Release
-	sudo make install
-	sudo ln -s /usr/local/bin/nvim /usr/bin/nvim
-    sudo update-alternatives --install /usr/bin/editor editor /usr/local/bin/nvim 1
-    sudo update-alternatives --set editor nvim
+	echo "raspberry pi detected"
+    sh ./install-scripts/raspbian.sh
+    configure_nvim
+    install_zsh
+    install_docker_engine
+elif [ "$(uname -s)" = "Darwin" ] ; then
+    echo "mac detected" 
+    # TODO: handle mac
 fi
-
-mkdir -p ~/.config
-sudo rm -rf ~/.config/nvim
-ln -s ~/dev-env/nvim ~/.config
-
-nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
-
-# install zsh
-sudo apt-get update && sudo apt-get upgrade 
-sudo apt-get -y install zsh
-sudo chsh -s /bin/zsh $USER
-
-# install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-sudo rm -rf ~/.zshrc
-sudo rm -rf ~/.zshrc.pre-oh-my-zsh
-ln -s ~/dev-env/shell/.zshrc ~/.zshrc
 
