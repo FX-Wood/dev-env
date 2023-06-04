78718115a2fc0c397c0c2c52b57b36cf997a8e2a
FX Wood <fxwood2@gmail.com>
2023-06-04 14:58:04 -0700

    outline mac stuff

--- setup.sh
+++ setup.sh
@@ -21,11 +21,18 @@ install_zsh() {
     ln -s ~/.dotfiles/shell/.zshrc ~/.zshrc
 }
 
+mac_set_keyboard_repeat() {
+    echo "setting keyboard repeat speed, make sure to logout and log back in"
+    defaults write -g InitialKeyRepeat -int 10 # normal minimum is 15 (225 ms)
+    defaults write -g KeyRepeat -int 1 # normal minimum is 2 (30 ms)
+}
+
 install_docker_engine() {
     echo "installing docker"
     sh -c "$(curl -fsSL https://get.docker.com)"
     sudo usermod -aG docker $USER
 }
+
 if grep -iq "raspberry" /proc/device-tree/model > /dev/null 2>&1; then
 	echo "raspberry pi detected"
     sh ./install-scripts/neovim-raspbian.sh
@@ -34,6 +41,15 @@ if grep -iq "raspberry" /proc/device-tree/model > /dev/null 2>&1; then
     install_docker_engine
 elif [ "$(uname -s)" = "Darwin" ] ; then
     echo "mac detected" 
-    # TODO: handle mac
+    # install homebrew
+    # install zsh (with homebrew)
+    # add zsh to /etc/shells
+    # install oh-my-zsh (curl command)
+    # install iterm (this one might be tough)
+    # change keyboard character repeat speed to "fast"
+    mac_set_keyboard_repeat
+    # delete zshrc
+    # link .dotfiles zshrc to home one
+
 fi
 
