c3b0bce433f08c8e7c307042272d24175c37bf17
FX Wood <fxwood2@gmail.com>
2024-02-22 23:43:10 -0800

    resurrect prime's keymap util

---
+++ nvim/lua/utils/keymap.lua
@@ -0,0 +1,25 @@
+-- shamelessly stolen from the primeagen
+-- https://github.com/ThePrimeagen/.dotfiles/blob/61784572c81d7a1973f6c0a2a4930c6739c89cc9/nvim/.config/nvim/lua/theprimeagen/keymap.lua
+local M = {}
+
+local function bind(op, outer_opts)
+    outer_opts = outer_opts or {noremap = true}
+    return function(lhs, rhs, opts)
+        opts = vim.tbl_extend("force",
+            outer_opts,
+            opts or {}
+        )
+        vim.keymap.set(op, lhs, rhs, opts)
+    end
+end
+
+M.nmap = bind("n", {noremap = false})
+M.nnoremap = bind("n")
+M.vnoremap = bind("v")
+M.xnoremap = bind("x")
+M.inoremap = bind("i")
+M.tnoremap = bind("t")
+M.noremap = bind("")
+M.noremap_bang = bind("!")
+
+return M
